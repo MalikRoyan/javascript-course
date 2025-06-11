d054f47a22ba336313dae7302bfd22c0f9cccd4e
MalikRoyan <129622359+MalikRoyan@users.noreply.github.com>
2025-06-11 21:09:07 +0000

    variables

---
+++ js course/test.js
@@ -0,0 +1,1 @@
+console.log("Royan Ahmad")
---
+++ js course/variables.js
@@ -0,0 +1,9 @@
+const accountId = 145
+let accountEmail = "royan@google.com"
+var accountPass = "12345"
+
+accountCity = "Multan"
+// accountId=145 not allowed to change the value of const
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPass, accountCity])
