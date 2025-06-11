72bd27e48a759b6b293d24615e932dfffc2469c4
MalikRoyan <129622359+MalikRoyan@users.noreply.github.com>
2025-06-11 21:39:03 +0000

    datatypes

---
+++ js course/data_types.js
@@ -0,0 +1,22 @@
+let name ="Royan"
+let age =20
+let isLoggedIn=true
+
+
+// Primitive data types
+
+// number (int)  => 2 to power of 53
+// bigint
+// string
+// boolean=> true or false
+// null => empty value
+// undefined=> when value is not yet initialized to the variable after declaration.
+// symbol => when we talk about unique
+
+// Non primitive data types
+
+// object
+// 
+// 
+
+ 
