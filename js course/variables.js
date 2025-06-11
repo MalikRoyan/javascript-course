484d342f36e2d95c19e2f4dd6f2df4f074482ccb
MalikRoyan <129622359+MalikRoyan@users.noreply.github.com>
2025-06-11 21:14:15 +0000

    variables

--- js course/variables.js
+++ js course/variables.js
@@ -2,8 +2,11 @@ const accountId = 145
 let accountEmail = "royan@google.com"
 var accountPass = "12345"
 
+// prefer not to use var in js due to its issues in scope
+
 accountCity = "Multan"
 // accountId=145 not allowed to change the value of const
+let accountState;
 
 console.log(accountId)
 console.table([accountId, accountEmail, accountPass, accountCity])
